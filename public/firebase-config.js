3aa22342e6ccf1077ffe23ac615d756d721d9619
gabriel-senai-dev <gabriel.l.bispo@ba.estudante.senai.br>
2025-05-16 09:59:39 -0300

    teste1

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,6 +1,6 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js"; // ✅ ESSENCIAL
 
 const firebaseConfig = {
   apiKey: "AIzaSyD583M1OZeoNOyqxuwf9zSEFzElEwub3y0",
@@ -11,10 +11,9 @@ const firebaseConfig = {
   appId: "1:610593951973:web:7d00e519af59ad5611440c",
   measurementId: "G-7SJKRQEYWR"
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app); // ✅ necessário para usar Firestore
 
-export { auth };
-
-
-
+export { auth, db, collection, getDocs }; // exporta funções para usar em outro script
