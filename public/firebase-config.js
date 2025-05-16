a84874dc09f9ba8300bdf4d2f619efd6b364bc0f
gabriel-senai-dev <gabriel.l.bispo@ba.estudante.senai.br>
2025-05-16 10:34:45 -0300

    teste1234567

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,6 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { 
+  getFirestore, 
+  collection, 
+  getDocs, 
+  addDoc,         
+  doc,
+  getDoc,
+  updateDoc,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD583M1OZeoNOyqxuwf9zSEFzElEwub3y0",
@@ -8,7 +17,7 @@ const firebaseConfig = {
   projectId: "loja-de-celulares",
   storageBucket: "loja-de-celulares.appspot.com",
   messagingSenderId: "610593951973",
-  appId: "1:610593951973:web:7d00e519af59ad5611440c",
+  appId: "1:610593951973:web:7d00e519ad5611440c",
   measurementId: "G-7SJKRQEYWR"
 };
 
@@ -16,4 +25,14 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, collection, getDocs };
+export { 
+  auth, 
+  db, 
+  collection, 
+  getDocs, 
+  addDoc,      
+  doc,
+  getDoc,
+  updateDoc,
+  deleteDoc
+};
